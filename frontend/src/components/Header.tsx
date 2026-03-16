26139eac6abcafa895b0c17ded8056eaa2f5a7dc
shivam <shivamg@finfactor.in>
2026-03-16 20:18:17 +0530

    uploading files

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,45 @@
+import { Link } from "react-router-dom";
+import { useAppContext } from "../contexts/AppContext";
+import SignOutButton from "./SignOutButton";
+
+const Header = () => {
+  const { isLoggedIn } = useAppContext();
+
+  return (
+    <div className="bg-purple-700 py-6">
+      <div className="container mx-auto flex justify-between">
+        <span className="text-3xl text-white font-bold tracking-tight">
+          <Link to="/">MernStays.com</Link>
+        </span>
+        <span className="flex space-x-2">
+          {isLoggedIn ? (
+            <>
+              <Link
+                className="flex items-center text-white px-3 font-bold hover:bg-purple-500"
+                to="/my-bookings"
+              >
+                My Bookings
+              </Link>
+              <Link
+                className="flex items-center text-white px-3 font-bold hover:bg-purple-500"
+                to="/my-hotels"
+              >
+                My Hotels
+              </Link>
+              <SignOutButton />
+            </>
+          ) : (
+            <Link
+              to="/sign-in"
+              className="flex bg-white items-center text-purple-700 px-3 font-bold hover:bg-gray-100"
+            >
+              Sign In
+            </Link>
+          )}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
