85cce97e7a681cc58bfc4eaf9e19ed3e67193964
shivam <shivamg@finfactor.in>
2026-03-24 14:44:12 +0530

    UI changes

--- frontend/src/components/PriceFilter.tsx
+++ frontend/src/components/PriceFilter.tsx
@@ -16,9 +16,9 @@ const PriceFilter = ({ selectedPrice, onChange }: Props) => {
           )
         }
       >
-        <option value="">Select Max Price</option>
-        {[50, 100, 200, 300, 500].map((price) => (
-          <option value={price}>{price}</option>
+        <option value="">Select Max Price (₹)</option>
+        {[500, 1000, 2000, 3000, 5000, 10000].map((price) => (
+          <option value={price}>{`₹${price}`}</option>
         ))}
       </select>
     </div>
